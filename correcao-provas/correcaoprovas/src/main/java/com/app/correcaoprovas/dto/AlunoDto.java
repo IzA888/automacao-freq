aa2d21695d428a75b73f432ed5490e6aa3bbbc64
IzabellaSilva <iza.americo@gmail.com>
2025-09-12 10:03:33 -0300

    add: dto para as entitys

---
+++ correcao-provas/correcaoprovas/src/main/java/com/app/correcaoprovas/dto/AlunoDto.java
@@ -0,0 +1,22 @@
+package com.app.correcaoprovas.dto;
+
+import java.util.List;
+
+import org.springframework.validation.annotation.Validated;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Validated
+public class AlunoDto {
+ 
+    private String nome;
+
+    private List<NotasDto> notas;
+}
---
+++ correcao-provas/correcaoprovas/src/main/java/com/app/correcaoprovas/dto/ProvaDto.java
@@ -0,0 +1,24 @@
+package com.app.correcaoprovas.dto;
+
+import java.util.List;
+
+import org.springframework.validation.annotation.Validated;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Validated
+public class ProvaDto {
+
+    private String materia;
+
+    private String gabarito;
+
+    private List<NotasDto> notas;
+}
