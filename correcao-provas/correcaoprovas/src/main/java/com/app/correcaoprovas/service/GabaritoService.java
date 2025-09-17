d8b145821dba20843467f59fa6acae4516e8e39f
IzabellaSilva <iza.americo@gmail.com>
2025-09-17 09:23:18 -0300

    feat: lógica para separar as questoes marcadas do gabarito

--- correcao-provas/correcaoprovas/src/main/java/com/app/correcaoprovas/service/GabaritoService.java
+++ correcao-provas/correcaoprovas/src/main/java/com/app/correcaoprovas/service/GabaritoService.java
@@ -1,16 +1,18 @@
 package com.app.correcaoprovas.service;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
 import javax.swing.JOptionPane;
 
+import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.opencv_core.Mat;
-import org.bytedeco.opencv.opencv_core.MatVector;
 import org.bytedeco.opencv.opencv_core.Rect;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class GabaritoService {
@@ -24,28 +26,40 @@ public class GabaritoService {
         }
     }
    
-    public static List<Rect> gabaritoLista(){
-    
+    public static List<String> gabaritoLista(Mat src, List<String> alternativas){
+        Map<Integer, List<Rect>> linhas = (Map<Integer, List<Rect>>) ArquivoService.encontrarBolhas(src);
+
+        
         int questao = 1;
-        for (Map.Entry<Integer, List<Rect>> entry > linhas linhas.entrySet()){
+        List<String> questoesMarcadas = new ArrayList<>();
+
+        for (Map.Entry<Integer, List<Rect>> entry : linhas.entrySet()){
             List<Rect> linha = entry.getValue();
+            //Ordenar da esquerda-direita
             linha.sort(Comparator.comparingInt(Rect::x));
     
-            for (int i=0; i< linha.size() && i< alternativas.length; i++){
+            for (int i=0; i< linha.size() && i< alternativas.size(); i++){
                     Rect r = linha.get(i);
+
+                    //Recorta a bolha
+                    Mat bolha = new Mat(src, r);
     
+                    //Conta quantos pixels estão pretos
                     double cheio = opencv_core.countNonZero(bolha);
-                    double area = rect.width() * rect.height();
+                    double area = r.width() * r.height();
                     double proporcao = cheio/area;
     
                     // Se mais da metade estiver preenchida, consideramos "marcada"
-                    Boolean marcada = proporcao > 0.5;
-                    List<Rect> gabaritoLayout;
-                    return gabaritoLayout.add(new marcadas(questoes, alternativas[i], r, marcadas));
+                    if( proporcao > 0.5){
+                        //Salva resultado
+                        questoesMarcadas.add(bolha.toString());
+
+                    }
     
                 }
-    
+                questao++;
         }
+        return questoesMarcadas;
 
     }
 
